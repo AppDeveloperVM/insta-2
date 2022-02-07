e5feb98a7b4d756a74c8c1c644a92e383831f2d9
VMTechy <vicmr1994@gmail.com>
2022-02-07 10:55:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# insta-v2.0
+ instagram version 2.0 react , tailwind
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Insta2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Instagram version 2.0</h1>
+    </div>
+  )
+}
